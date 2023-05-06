68998c7cb6538a71c903323a53323db6e17cdca3
Jean Meira <jean.meira10@hotmail.com>
2023-05-05 23:32:23 -0300

    feat: update home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,7 +4,23 @@ import Styles from "./styles.module.scss";
 export const Home: React.FC = () => {
   return (
     <div className={Styles.wrapper}>
-      <h1>Home</h1>
+      <div className={Styles.content}>
+        <div className={Styles.cloud}>
+          <div>
+            <h1>Porque sua roupa sempre tem algo a contar</h1>
+          </div>
+        </div>
+
+        <h1 className={Styles["contrat-white"]}>
+          E aqui, cada costura é uma história, e nós estamos prontos para contar
+          a sua através das suas roupas personalizadas.
+        </h1>
+      </div>
+
+      <div className={Styles["cta-wrapper"]}>
+        <div />
+        <button>Conheça nossa linhas</button>
+      </div>
     </div>
   );
 };
