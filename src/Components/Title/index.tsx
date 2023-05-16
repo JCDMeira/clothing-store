b9206c73f044ed051eacaa1e0d3091f317ee39b8
Jean Meira <jean.meira10@hotmail.com>
2023-05-15 21:25:10 -0300

    feat: add title as a component

---
+++ src/Components/Title/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+
+// import { Container } from './styles';
+
+type TitleProps = {
+  variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  children: ReactNode;
+};
+
+const Title: React.FC<TitleProps> = ({ variant, children, ...props }) => {
+  const Component = variant;
+  return <Component {...props}>{children}</Component>;
+};
+
+export default Title;
