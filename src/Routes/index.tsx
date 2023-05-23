887c832de95553fc5c86727caf57f45076a71670
Jean Meira <jean.meira10@hotmail.com>
2023-05-22 23:00:06 -0300

    feat: add lazy loading on routes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,22 +1,49 @@
+import { lazy } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Home, Product, ProductDetail } from "../Pages";
-import { ProductAdmin } from "../Pages/ProductAdmin";
-import { CreateProduct, EditProduct } from "../Pages/ProductAdmin/pages";
+import { useSuspense } from "../hooks";
+const Home = lazy(() =>
+  import("../Pages").then(({ Home }) => ({ default: Home }))
+);
+const Product = lazy(() =>
+  import("../Pages").then(({ Product }) => ({ default: Product }))
+);
+const ProductDetail = lazy(() =>
+  import("../Pages").then(({ ProductDetail }) => ({ default: ProductDetail }))
+);
+const ProductAdmin = lazy(() =>
+  import("../Pages/ProductAdmin").then(({ ProductAdmin }) => ({
+    default: ProductAdmin,
+  }))
+);
+const CreateProduct = lazy(() =>
+  import("../Pages/ProductAdmin/pages").then(({ CreateProduct }) => ({
+    default: CreateProduct,
+  }))
+);
+const EditProduct = lazy(() =>
+  import("../Pages/ProductAdmin/pages").then(({ EditProduct }) => ({
+    default: EditProduct,
+  }))
+);
 
 export const RouteSelect = () => {
+  const { wrappInSuspense } = useSuspense();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/" element={wrappInSuspense(<Home />)} />
+        <Route path="/products" element={wrappInSuspense(<Product />)} />
+        <Route
+          path="/products/:id"
+          element={wrappInSuspense(<ProductDetail />)}
+        />
 
         <Route path="/admin">
-          <Route path="" element={<ProductAdmin />} />
-          <Route path="create" element={<CreateProduct />} />
-          <Route path="edit/:id" element={<EditProduct />} />
+          <Route path="" element={wrappInSuspense(<ProductAdmin />)} />
+          <Route path="create" element={wrappInSuspense(<CreateProduct />)} />
+          <Route path="edit/:id" element={wrappInSuspense(<EditProduct />)} />
         </Route>
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={wrappInSuspense(<Home />)} />
       </Routes>
     </BrowserRouter>
   );
